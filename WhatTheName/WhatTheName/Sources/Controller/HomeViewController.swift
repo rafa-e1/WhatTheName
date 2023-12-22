71ae7ef79f21529b222005379141425122cb89aa
Rafa <rafa71802937@gmail.com>
2023-12-22 17:32:56 +0900

    init: 프로젝트 생성 및 초기 세팅 완료

---
+++ WhatTheName/WhatTheName/Sources/Controller/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  WhatTheName
+//
+//  Created by Rafael on 12/22/23.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
